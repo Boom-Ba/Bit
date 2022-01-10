3c1a26861cd5e37dd194c26562b0fec4a4db034c
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-09 22:36:35 -0500

    Update Bitwise_And_In_Range.py

--- Bitwise_And_In_Range.py
+++ Bitwise_And_In_Range.py
@@ -1,3 +1,21 @@
+#given a range (M,N) =(2,5) inclusive, find bitwise AND
+#res= 2&3&4&5 
+#Bitwise and returns 0 if bit flips happen (bits contains 1 and 0 at position)
+#if a column never flipped, the value should be copied directly 
+
+M =13
+N =16
+##observation: when there is a new bit add to the leftmost
+##all its right bits got flipped as well 
+#  0000 - 0
+#  0001 -1
+#  0010 -2
+#  0011 -3
+## thid bit got flipped from 0 to 1, all right bits also flipped to 0
+#  0100 -4
+#  0101 -5
+#  0110 -6
+#  0111 -7
 #16-10000 --1 bit shift to right -1000
 #19-10011 
 #common bits is 100 = 4
